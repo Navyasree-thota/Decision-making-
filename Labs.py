a5453f5078cb1a1cb20c610ba634c591dc7d2f95
Navyasree-thota <nandunavyasri@gmail.com>
2024-10-02 08:12:13 +0530

    Create Labs.py

---
+++ Labs.py
@@ -0,0 +1,38 @@
+,,,
+There are 3 labs in the CSE department are L1, L2, and L3 with a seating capacity of x, y, and z. A single lab needs to be allocated to a class of 'n' students. How many of the 3 labs can accommodate 'n' students?
+Input format:
+Input consists of 4 integers
+The first input denotes the seating capacity of L1(a)
+The second input denotes the seating capacity of L2(b)
+The third input denotes the seating capacity of L3(c)
+The fourth input denotes the number of students(x)
+Output format:
+Print the number of labs which can accommodate the 'n' number of students
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+2 
+10)
+
+ There are 3 labs in the CSE department. The labs are L1, L2, and L3 with a seating capacity of x, y, and z respectively. A single lab needs to be allocated to a class of 'n' students. The labs need to be utilized to the maximum i.e the number of systems used should not be minimal. Which lab needs to be allocated to this class?
+Input format:
+Input consists of 4 integers
+The first input denotes 'x'
+The second input denotes 'y'
+The third input denotes 'z'
+The fourth input denotes 'n'
+Output format:
+Print the lab which is suitable for  'n' number of students
+Refer the Sample output for formatting
+Sample Input:
+30
+40
+20
+25
+Sample Output:
+L1
+,,,
